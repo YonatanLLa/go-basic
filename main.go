c70010fe4a219870491c9f4808d7374e8ad76209
yonatanlla <yonatanllanto174@gmail.com>
2023-09-09 17:27:51 -0500

    hola mundo go

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+// admite solo un paque
+import (
+	"go/src/variables"
+)
+
+// function principal de go
+func main()  {
+	variables.MuestroEntero()
+}
---
+++ variables/enteras.go
@@ -0,0 +1,16 @@
+package variables
+
+import "fmt"
+
+// function public
+func MuestroEntero()  {
+	var intcomun int 
+	intde32 := int32(10)
+
+	intde64 := int64(100)
+
+	fmt.Println("intcomun = ", intcomun)
+	fmt.Println("intcomun = ", intde32)
+	fmt.Println("intcomun = ", intde64)
+	
+}
